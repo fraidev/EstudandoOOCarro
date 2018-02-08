1a384e4f910170bf3867a09869c7343d3c343c0c
Felipe Cardozo <fraifelipe@gmail.com>
2018-02-08 14:59:06 -0200

    OO

---
+++ EstudandoOOCarro/Carro.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudandoOOCarro
+{
+    public interface IAutomovel
+    {
+        Direcao Direcao { get; }
+        void VirarParaDireita();
+        void VirarParaEsquerda();
+    }
+
+    public class Carro : IAutomovel
+    {
+        public Carro()
+        {
+            LeftOrRight = 0;
+        }
+
+        public Direcao Direcao
+        {
+            get
+            {
+                if (LeftOrRight > 0)
+                {
+                    return Direcao.Direita;
+                }
+
+                if (LeftOrRight < 0)
+                {
+                    return Direcao.Esquerda;
+                }
+
+                return Direcao.Frente;
+            }
+        }
+
+        private int LeftOrRight { get; set; }
+
+        public void VirarParaDireita()
+        {
+            LeftOrRight++;
+        }
+
+        public void VirarParaEsquerda()
+        {
+            LeftOrRight--;
+        }
+
+    }
+
+    public class BarraDirecao
+    {
+        private readonly int _maxUso;
+
+        public BarraDirecao(int maxUso)
+        {
+            _maxUso = maxUso;
+        }
+        
+        public Estado Estado
+        {
+            get
+            {
+                if (CountUso < _maxUso)
+                {
+                    return Estado.Funcionando;
+                }
+                else
+                {
+                    return Estado.Quebrado;
+                }
+            }
+        }
+
+        
+        private int CountUso;
+
+        public void Usar()
+        {
+            CountUso++;
+        }
+    
+
+
+    }
+
+    public enum Estado
+    {
+        Funcionando,
+        Quebrado
+    }
+
+    public enum Direcao
+    {
+        Frente,
+        Esquerda,
+        Direita
+    }
+}
---
+++ EstudandoOOCarroTeste/CarroTeste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstudandoOOCarro;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EstudandoOOCarroTeste
+{
+    public class CarroTeste
+    {
+        [TestCase(0, 0, Direcao.Frente)]
+        [TestCase(1, 0, Direcao.Esquerda)]
+        [TestCase(0, 1, Direcao.Direita)]
+        [TestCase(2, 1, Direcao.Esquerda)]
+        [TestCase(1, 2, Direcao.Direita)]
+        [TestCase(2, 2, Direcao.Frente)]
+        public void Teste_da_barra_de_direcao_do_carro(int voltasParaEsquerda, int voltasParaDireita, Direcao esperado)
+        {
+            //Get
+            var carro = new Carro();
+
+            // When 
+            for (var i = 0; i < voltasParaEsquerda; i++)
+            {
+                carro.VirarParaEsquerda();
+            }
+
+            for (var i = 0; i < voltasParaDireita; i++)
+            {
+                carro.VirarParaDireita();
+            }
+
+            // Then
+            carro.Direcao.Should().Be(esperado);
+        }
+    }
+}
